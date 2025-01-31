c15b43f216778cd0fcf9707f72754055e9295694
Елизавета <166805270+lizaesakova@users.noreply.github.com>
2025-01-31 17:28:36 +1000

    Update comlexn.py

--- Complexnum/comlexn.py
+++ Complexnum/comlexn.py
@@ -3,104 +3,127 @@ from rational import Rational
 
 class ComplexNumber:
 
-
-    def __init__(self, real: Rational | None, imag: Rational | None):
-        if not isinstance(real, Rational):
-            raise ValueError("real part must be a rational number.")
-        if not isinstance(imag, Rational):
-            raise ValueError("imaginary part must be a rational number.")
+    def __init__(self, real: int | Rational | float, imag: int | Rational | float=0):
+        
         self.__real = real
         self.__imag = imag
 
     @property
-    def real(self):
+    def real(self) -> Rational:
         return self.__real
 
     @real.setter
-    def real(self, value: Rational | None):
+    def real(self, value: Rational):
         if not isinstance(value, Rational):
-            raise ValueError("real part must be a rational number.")
+            raise ValueError("Real part must be a rational number.")
         self.__real = value
 
     @property
-    def imag(self):
+    def imag(self) -> Rational:
         return self.__imag
 
     @imag.setter
-    def imag(self, value: Rational | None):
+    def imag(self, value: Rational):
         if not isinstance(value, Rational):
-            raise ValueError("imaginary part must be a rational number.")
+            raise ValueError("Imaginary part must be a rational number.")
         self.__imag = value
 
     def __add__(self, other):
-        if not isinstance(other, ComplexNumber):
-            raise TypeError("operand must be a complexnumber.")
-        return ComplexNumber(self.real + other.real, self.imag + other.imag)
+        if isinstance(other, ComplexNumber):
+            return ComplexNumber(self.real + other.real, self.imag + other.imag)
+        if isinstance(other, Rational | int | float):
+            return Complex(self.real + Rational(other), self.imag)
+        else:
+            raise TypeError("operand must be a complexnumber or a number.")
 
     def __iadd__(self, other):
-        if not isinstance(other, ComplexNumber):
-            raise TypeError("operand must be a complexnumber.")
-        self.real += other.real
-        self.imag += other.imag
+        if isinstance(other, ComplexNumber):
+            self.real += other.real
+            self.imag += other.imag
+        elif isinstance(other, (Rational, int, float)):
+            self.real += Rational(other)
+            self.imag += Rational(other)
+        else:
+            raise TypeError("operand must be a complexnumber or a number.")
         return self
 
     def __sub__(self, other):
-        if not isinstance(other, ComplexNumber):
-            raise TypeError("operand must be a complexnumber.")
-        return ComplexNumber(self.real - other.real, self.imag - other.imag)
-
+        if isinstance(other, ComplexNumber):
+            return ComplexNumber(self.real - other.real, self.imag - other.imag)
+        elif isinstance(other, (Rational, int, float)):
+            return ComplexNumber(self.real - Rational(other), self.imag)
+        else:
+            raise TypeError("operand must be a complexnumber or a number.")
+    
     def __isub__(self, other):
-        if not isinstance(other, ComplexNumber):
-            raise TypeError("operand must be a complexnumber.")
-        self.real -= other.real
-        self.imag -= other.imag
+        if isinstance(other, ComplexNumber):
+            self.real -= other.real
+            self.imag -= other.imag
+        elif isinstance(other, (Rational, int, float)):
+            self.real -= Rational(other)  # Преобразуем в Rational
+        else:
+            raise TypeError("operand must be a complex number or a number.")
         return self
 
     def __mul__(self, other):
-        if not isinstance(other, ComplexNumber):
-            raise TypeError("operand must be a complexnumber.")
-        real_part = self.real * other.real - self.imag * other.imag
-        imag_part = self.real * other.imag + self.imag * other.real
-        return ComplexNumber(real_part, imag_part)
+        if isinstance(other, ComplexNumber):
+            real_part = self.real * other.real - self.imag * other.imag
+            imag_part = self.real * other.imag + self.imag * other.real
+            return ComplexNumber(real_part, imag_part)
+        elif isinstance(other, (Rational, int, float)):
+            return ComplexNumber(self.real * Rational(other), self.imag * Rational(other))
+        else:
+            raise TypeError("operand must be a complexnumber or a number.")
 
     def __truediv__(self, other):
-        if not isinstance(other, ComplexNumber):
-            raise TypeError("operand must be a complexnumber.")
-        if other.real == 0 and other.imag == 0:
-            raise ZeroDivisionError("cannot divide by zero.")
-
-        denominator = other.real ** 2 + other.imag ** 2
-        real_part = (self.real * other.real + self.imag * other.imag) / denominator
-        imag_part = (self.imag * other.real - self.real * other.imag) / denominator
-        return ComplexNumber(real_part, imag_part)
-
+        if isinstance(other, ComplexNumber):
+            if other.real == 0 and other.imag == 0:
+                raise ZeroDivisionError("cannot divide by zero.")
+            denominator = other.real ** 2 + other.imag ** 2
+            real_part = (self.real * other.real + self.imag * other.imag) / denominator
+            imag_part = (self.imag * other.real - self.real * other.imag) / denominator
+            return ComplexNumber(real_part, imag_part)
+        elif isinstance(other, (Rational, int, float)):
+            if other == 0:
+                raise ZeroDivisionError("cannot divide by zero.")
+            return ComplexNumber(self.real / Rational(other), self.imag / Rational(other))
+        else:
+            raise TypeError("operand must be a complexnumber or a number.")
+        
     def __itruediv__(self, other):
-       if not isinstance(other, ComplexNumber):
-            raise TypeError("operand must be a complexnumber.")
-       if other.real == 0 and other.imag == 0:
-            raise ZeroDivisionError("cannot divide by zero.")
-
-
-       self.real = (self.real * other.real + self.imag * other.imag) / (other.real ** 2 + other.imag ** 2)
-       self.imag = (self.imag * other.real - self.real * other.imag) / (other.real ** 2 + other.imag ** 2)
-
-       return self
-
+        if isinstance(other, ComplexNumber):
+            if other.real == 0 and other.imag == 0:
+                raise ZeroDivisionError("cannot divide by zero.")
+            denominator = other.real ** 2 + other.imag ** 2
+            self.real = (self.real * other.real + self.imag * other.imag) / denominator
+            self.imag = (self.imag * other.real - self.real * other.imag) / denominator
+        elif isinstance(other, (Rational, int, float)):
+            if other == 0:
+                raise ZeroDivisionError("cannot divide by zero.")
+            self.real /= Rational(other)
+            self.imag /= Rational(other)
+        else:
+            raise TypeError("operand must be a complex number or a number.")
+        return self
+        
     def __eq__(self, other):
-        if not isinstance(other, ComplexNumber):
-            raise TypeError("operand must be a complexnumber")
-        return self.real == other.real and self.imag == other.imag
+        if isinstance(other, ComplexNumber):
+            return self.real == other.real and self.imag == other.imag
+        if isinstance(other, Rational | int | float):
+            return self.real == other and self.imag == 0
+        else:
+            raise TypeError("operand must be a complex number or a number.")
 
     def __ne__(self, other):
         return not self == other
 
-    def __neg__(self):
+    def __min__(self):
         return ComplexNumber(-self.real, -self.imag)
 
     def pow(self, n: int):
         if n < 0:
             raise ValueError("exponent must be a non-negative integer.")
-        result = ComplexNumber(Rational(1, 1), Rational(0, 1))  # 1 + 0i
+        result = ComplexNumber(Rational(1, 1), Rational(0, 1)) 
         for _ in range(n):
             result *= self
         return result
